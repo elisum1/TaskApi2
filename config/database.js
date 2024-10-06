a421ce3b23c8d8af98307f3a14a01e5703792057
Elias Urrieta <elias.um94@gmail.com>
2024-10-05 21:38:18 -0500

    primer commite

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 
 // Usando DB_URI para la conexión
 const sequelize = new Sequelize(process.env.DB_URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: console.log,
 });
 
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configurar CORS
 app.use(cors({
-  origin: ['http://localhost:5173'], 
+  origin: ['https://task-web2.vercel.app/'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
